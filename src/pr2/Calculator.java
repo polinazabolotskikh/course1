85d032b2a3fd0abb9b549d33dcb9561fa3aa4292
polinazabolotskikh <pzobolotik@gmail.com>
2023-12-11 16:31:17 +0400

    all to double

--- src/pr2/Calculator.java
+++ src/pr2/Calculator.java
@@ -38,7 +38,7 @@ public class Calculator {
         return x/y;
     }
     public double div(long x, long y){
-        return (x/y)+(x%y);
+        return (double)(x)/(double)(y);
     }
     public void enter(){
         System.out.println("\nСложение:");
